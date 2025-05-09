55c3b22a159060a4f08dd62ff55e86ad2227eb02
marina-fares <marina.fares11@gmail.com>
2025-05-09 18:40:16 +0000

    merge settings.py

--- backend/Config/settings.py
+++ backend/Config/settings.py
@@ -75,7 +75,8 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://fo.gravitycode.me',
+        'https://fo.gravitycode.me',
+    'https://fobook.gravitycode.me',
     'http://localhost:3001'
 ]
 
@@ -185,7 +186,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
