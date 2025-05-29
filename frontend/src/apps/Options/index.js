da6ff0379c0ab52d3ce9680b81cff7006f33b4e8
marina-fares <marina.fares11@gmail.com>
2025-05-29 17:00:17 +0300

    remove zoho api2

--- frontend/src/apps/Options/index.js
+++ frontend/src/apps/Options/index.js
@@ -244,7 +244,9 @@ export default function Options() {
 				{
 					set_zoho_sales_receipt_id(response.sales_receipt_details.sales_receipt_id)	
 				}
-			
+			else{
+				set_zoho_sales_receipt_id(true)
+			}
 			// else{
 			// 	setAlert(true)
 			// 	setMessage(response.message)
