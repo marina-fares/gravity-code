f51e03d35f56f9d793351ea3387307d54361650f
marina-fares <marina.fares11@gmail.com>
2025-06-09 22:04:15 +0300

    Revert "remove zoho api"
    This reverts commit c3f4b42e9b41be5eee5631fe74bf82be1ffcafad.

--- frontend/src/apps/Options/index.js
+++ frontend/src/apps/Options/index.js
@@ -245,10 +245,10 @@ export default function Options() {
 					set_zoho_sales_receipt_id(response.sales_receipt_details.sales_receipt_id)	
 				}
 			
-			// else{
-			// 	setAlert(true)
-			// 	setMessage(response.message)
-			// }
+			else{
+				setAlert(true)
+				setMessage(response.message)
+			}
 
 		})
 	}
